446b7d242db5510bab9302aa44f8db7103da1060
giacomotolari <tolarigiacomo@gmail.com>
2023-12-07 14:01:21 +0100

    useRef with DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
-import Post from "./components/Post";
 
-// Utilizing the children prop becomes valuable when you intend to employ the same component that can render different elements.
-// Moreover, this prop allows you to use another component as a child, offering flexibility and versatility in component composition.
+
+// With useRef we can access the DOM elements and style them like with DOM manipulation
 
 function App() {
+  const appRef = useRef<HTMLDivElement>(null);
+  const inputTextRef = useRef<HTMLInputElement>(null);
+  console.log("appRef-outside-useEffect:", appRef); // we get null because the component has not been rendered yet
+
+  useEffect(() => {
+    console.log("appRef-in-useEffect:", appRef); // we get the div element because the component has been rendered
+    inputTextRef.current?.focus(); // we focus the input element when the component has been rendered
+    appRef.current?.style.setProperty("color", "red"); // we change the color of the div element when the component has been rendered
+  }, []);
+
   return (
-    <>
+    <div ref={appRef}>
       {/* use the Post component without props.children */}
-      <Post
-        title="Post 1"
-        text="93248328432984 dsfdsjfdkjf dfjkdsjflkdsfj sdlkjfldsfldsfkjdsf sdfdskjfdsjflsdf lksjdflkdsjflkjds"
-      />
-      {/* use the Post component with props.children */}
-      <Post
-        title="Post 2"
-        text="93248328432984 dsfdsjfdkjf dfjkdsjflkdsfj sdlkjfldsfldsfkjdsf sdfdskjfdsjflsdf lksjdflkdsjflkjds"
-      >
-        {/* child of Post */}
-        <p>Some element 1</p>
-        {/* child of Post */}
-        <h3>Some element 2</h3>
-      </Post>
-    </>
+      <h1>useRef</h1>
+      <label htmlFor="inputText">Please, write your name</label>
+      <input ref={inputTextRef} type="text" id="inputText" />
+    </div>
   );
 }
 
