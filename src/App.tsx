a9e2aa81926ba761fed41c076dbfdedbca46dbdf
giacomotolari <tolarigiacomo@gmail.com>
2023-12-11 13:10:21 +0100

    task completed, add comments to explain why use useRef fpr manipualate the dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useRef } from "react";
 import "./App.css";
 
-
 // With useRef we can access the DOM elements and style them like with DOM manipulation
+// Is very important to understand why we use useRef to access the DOM elements and not use the DOM manipulation directly
+// The reason is:
+// If we use DOM selectors, we will select the DOM elements for each re-render of the component, with useRef we select the DOM elements only once
 
 function App() {
   const appRef = useRef<HTMLDivElement>(null);
@@ -12,7 +14,6 @@ function App() {
   useEffect(() => {
     console.log("appRef-in-useEffect:", appRef); // we get the div element because the component has been rendered
     inputTextRef.current?.focus(); // we focus the input element when the component has been rendered
-    appRef.current?.style.setProperty("color", "red"); // we change the color of the div element when the component has been rendered
   }, []);
 
   return (
@@ -21,6 +22,11 @@ function App() {
       <h1>useRef</h1>
       <label htmlFor="inputText">Please, write your name</label>
       <input ref={inputTextRef} type="text" id="inputText" />
+      {/* we change the color of the div element when the component has been rendered (we can use "setProperty" or use the name of the css property to change*/}
+      {/* <button onClick={() => appRef.current?.style.setProperty("backgroundColor", "blue")}> */}
+      <button onClick={() => (appRef.current!.style.backgroundColor = "blue")}>
+        update background
+      </button>
     </div>
   );
 }
